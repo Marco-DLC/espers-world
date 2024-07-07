2eed540335f98afad5fc26a99f4a09abc7a37f49
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-07 15:21:21 -0400

    Add Header with site name and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Header.jsx'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header(){
+    return (
+    <div className="header">
+        <img src="./images/dryad.webp" className="icon-dryad" />
+        <h1>Esper&apos;s World</h1>
+        <img src="./images/dryad.webp" className="icon-dryad reverse" />
+    </div>
+    )
+}
+
