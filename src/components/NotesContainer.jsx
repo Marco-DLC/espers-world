50b3eeb004b248c2e64404819dd26f09453574a6
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-09 20:34:24 -0400

    Add function to auto-focus on visible inputs
    When user clicks a p element for either the note's title or content, the
    input will autofocus so users can start typing right away. This is also
    great because if a user isn't focus on an input it will always be a p
    element, with the initial note content input as an exception.

--- src/components/NotesContainer.jsx
+++ src/components/NotesContainer.jsx
@@ -17,41 +17,44 @@ export default function NotesContainer({ children }) {
 }
 
 class Note extends Component {
-  constructor () {
+  constructor() {
     super();
 
     this.state = {
       note: {
-        title: 'Note',
-        note: ""
+        title: "Note",
+        note: "",
       },
       isVisible: {
         title: true,
-        note: false
-      }
-    }
-    
+        note: false,
+      },
+    };
+
+    this.titleInputRef = React.createRef();
+    this.noteInputRef = React.createRef();
 
     this.handleChange = this.handleChange.bind(this);
     this.toggleVisibility = this.toggleVisibility.bind(this);
     this.handleEnterKey = this.handleEnterKey.bind(this);
+    this.focusOnInput = this.focusOnInput.bind(this);
   }
 
   handleChange(e) {
     if (e.target.className === "title-input") {
-      this.setState(prevState => ({
+      this.setState((prevState) => ({
         note: {
-         ...prevState.note, 
-         title: e.target.value
-        }
-         }));
+          ...prevState.note,
+          title: e.target.value,
+        },
+      }));
     } else {
-      this.setState(prevState => ({
+      this.setState((prevState) => ({
         note: {
-         ...prevState.note,
-          note: e.target.value
-        }
-         }));
+          ...prevState.note,
+          note: e.target.value,
+        },
+      }));
     }
   }
 
@@ -63,52 +66,62 @@ class Note extends Component {
     };
   }
 
-  toggleVisibility(field){
-    this.setState(prevState => ({
+  toggleVisibility(field) {
+    this.setState((prevState) => ({
       isVisible: {
-       ...prevState.isVisible, 
-       [field]: !prevState.isVisible[field] 
-      }
-      }));
+        ...prevState.isVisible,
+        [field]: !prevState.isVisible[field],
+      },
+    }), () => {this.focusOnInput(field)});
+  }
+
+  focusOnInput(field) {
+    if (field === 'title' && this.titleInputRef.current) {
+      this.titleInputRef.current.focus();
+    } else if (field === 'note' && this.noteInputRef.current) {
+      this.noteInputRef.current.focus();
+    }
   }
 
   render() {
     const { note, isVisible } = this.state;
 
-  return (
-    <div className="note">
-      {isVisible.title ? (
-        <p className="title" onClick={() => this.toggleVisibility("title")}>
-          {note.title.trim() === "" ? "Note" : note.title}
-        </p>
-      ) : (
-        <input
-          type="text"
-          value={note.title}
-          className="title-input"
-          name="title"
-          onBlur={() => this.toggleVisibility('title')}
-          onChange={this.handleChange}
-          onKeyDown={this.handleEnterKey("title")}
-        />
-      )}
+    return (
+      <div className="note">
+        {isVisible.title ? (
+          <p className="title" onClick={() => this.toggleVisibility("title")}>
+            {note.title.trim() === "" ? "Note" : note.title}
+          </p>
+        ) : (
+          <input
+            type="text"
+            value={note.title}
+            className="title-input"
+            ref={this.titleInputRef}
+            name="title"
+            onBlur={() => this.toggleVisibility("title")}
+            onChange={this.handleChange}
+            onKeyDown={this.handleEnterKey("title")}
+          />
+        )}
 
-      {isVisible.note ? (
-        <p className="note" onClick={() => this.toggleVisibility("note")}>
-          {note.note.trim() == "" ? "(Empty)" : note.note}
-        </p>
-      ) : (
-        <textarea
-          value={note.note}
-          name="note"
-          className="note-input"
-          placeholder="Write your thoughts here..."
-          onBlur={() => this.toggleVisibility('note')}
-          onChange={this.handleChange}
-          onKeyDown={this.handleEnterKey("note")}
-        />
-      )}
-    </div>
-  );
-}
+        {isVisible.note ? (
+          <p className="note" onClick={() => this.toggleVisibility("note")}>
+            {note.note.trim() == "" ? "(Empty)" : note.note}
+          </p>
+        ) : (
+          <textarea
+            value={note.note}
+            name="note"
+            className="note-input"
+            ref={this.noteInputRef}
+            placeholder="Write your thoughts here..."
+            onBlur={() => this.toggleVisibility("note")}
+            onChange={this.handleChange}
+            onKeyDown={this.handleEnterKey("note")}
+          />
+        )}
+      </div>
+    );
+  }
 }
